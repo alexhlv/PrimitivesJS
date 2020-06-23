6f800e8f031fc320f32add696fb49e9f3ec5c13f
alexhlv <alexhlv@gmail.com>
2020-06-23 17:40:20 +0300

    All ten tasks on JavaScript - Primitives are ready

---
+++ assets/js/main.js
@@ -0,0 +1,123 @@
+/* Завдання 1:*/
+alert('Привіт світ!');
+console.log('Привіт світ!');
+/* Завдання 1:*/
+/* Завдання 2:*/
+let a = 8;
+let b = 4;
+
+let sub = a - b;
+console.log(sub);
+
+let add = a + 2;
+console.log(add);
+
+let mult = a * b;
+console.log(mult);
+
+let div = a / b;
+console.log(div);
+
+let rem = a % b;
+console.log(rem);
+
+let pow = a ** b;
+console.log(pow);
+/* Завдання 2:*/
+/* Завдання 3:*/
+let c = prompt('Введіть перше число');
+let d = prompt('Введіть друге число');
+
+let result = c / d;
+console.log(result);
+/* Завдання 3:*/
+/* Завдання 4:*/
+let e = +prompt('Введіть перше число');
+let f = 10;
+
+let result1 = (e >= f);
+alert(result1);
+
+let result2 = (e <= f);
+alert(result2);
+/* Завдання 4:*/
+/* Завдання 5:*/
+let height = 23;
+let width = 10;
+
+let s = height * width;
+console.log(s);
+
+let res5 = (s + 'см');
+console.log(res5);
+/* Завдання 5:*/
+/* Завдання 6:*/
+let name = 'Alex';
+let surname = 'Kholyavko';
+let city = 'Vinnytsia';
+let country = 'Ukraine';
+console.log(`Hi, my name is ${name} ${surname}, I live in ${country}, the city is ${city}`);
+
+let myInfo = `Hi, my name is ${name} ${surname}, I live in ${country}, the city is ${city}`;
+console.log(myInfo);
+/* Завдання 6:*/
+/* Завдання 7:*/
+let myName = prompt('Введіть своє Ім\'я');
+let mySurname = prompt('Введіть своє Прізвище');
+let myCity = prompt('Введіть своє Місто');
+let myCountry = prompt('Введіть свою Країну');
+console.log(`Hi, my name is ${myName} ${mySurname}, I live in ${myCountry}, the city is ${myCity}`);
+
+let myProfile = `Hi, my name is ${myName} ${mySurname}, I live in ${myCountry}, the city is ${myCity}`;
+console.log(myProfile);
+/* Завдання 7:*/
+/* Завдання 8:*/
+let x1 = 3;
+let y1 = 20;
+
+let res81 = (72 * y1);
+console.log(res81);
+
+let res82 = (22 * x1);
+console.log(res82);
+
+let res83 = res81 + res82;
+console.log(res83);
+
+let res84 = res83 / 4;
+console.log(res84);
+
+let res85 = res84 + 232;
+console.log('Результат', res85);
+/* Завдання 8:*/
+/* Завдання 9:*/
+let x2 = 16;
+let y2 = 20;
+
+let res91 = (100 * y2);
+console.log(res91);
+
+let res92 = (x2 / 5);
+console.log(res92);
+
+let res93 = res91 / 2;
+console.log(res93);
+
+let res94 = 5 * res92;
+console.log(res94);
+
+let res95 = res93 + res94 - 5;
+console.log(res95);
+
+let res96 = res95 * 6;
+console.log('Результат', res96);
+/* Завдання 9:*/
+/* Завдання 10:*/
+let r = 15;
+let h = 25;
+
+let V10 = (Math.PI * r ** r * h);
+console.log('Об\'єм циліндру', V10);
+let S10 = 2 * Math.PI * r * (r + h);
+console.log('Площа циліндру', S10);
+/* Завдання 10:*/
